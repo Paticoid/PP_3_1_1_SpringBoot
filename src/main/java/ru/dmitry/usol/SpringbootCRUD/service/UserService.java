dc5cdb4e913fa6d770b55102e6342cb0da7279e5
Paticoid <usolkin.dima@yandex.ru>
2022-11-02 11:49:04 +0300

    corrected project

--- src/main/java/ru/dmitry/usol/SpringbootCRUD/service/UserService.java
+++ src/main/java/ru/dmitry/usol/SpringbootCRUD/service/UserService.java
@@ -3,8 +3,9 @@ package ru.dmitry.usol.SpringbootCRUD.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.dmitry.usol.SpringbootCRUD.dao.UserDaoInt;
 import ru.dmitry.usol.SpringbootCRUD.models.User;
-import ru.dmitry.usol.SpringbootCRUD.dao.UserRepositoryDao;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -12,29 +13,27 @@ import java.util.Optional;
 @Service
 @Transactional(readOnly = true)
 public class UserService implements UserServiceInter {
-    private UserRepositoryDao userRepositoryDao;
+    private UserDaoInt userDaoInt;
     @Autowired
-    public UserService(UserRepositoryDao userRepositoryDao) {
-        this.userRepositoryDao = userRepositoryDao;
+    public UserService(UserDaoInt userDaoInt) {
+        this.userDaoInt = userDaoInt;
     }
     public List<User> allUsers() {
-         return userRepositoryDao.findAll();
+         return userDaoInt.allUsers();
     }
     public User show(long id) {
-        Optional<User> user = userRepositoryDao.findById(id);
-        return user.orElse(null);
+        return userDaoInt.show(id);
     }
     @Transactional
     public void save(User user) {
-        userRepositoryDao.save(user);
+        userDaoInt.save(user);
     }
     @Transactional
     public void update(long id,User updateUser) {
-        updateUser.setId(id);
-        userRepositoryDao.save(updateUser);
+        userDaoInt.update(id,updateUser);
     }
     @Transactional
     public void delete(long id) {
-        userRepositoryDao.deleteById(id);
+        userDaoInt.delete(id);
     }
 }
